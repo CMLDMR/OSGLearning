11b85e12fab1753950aaa8345b89c219b9eee09a
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-16 11:19:26 +0300

    added new

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <QtCore/QCoreApplication>
+#include <QtCore/qglobal.h>
+
+#include <QDir>
+
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+    osg::ref_ptr<osg::Node> root = osgDB::readNodeFile("cessna.osg");
+
+
+
+    osgViewer::Viewer viewer;
+
+
+    viewer.setSceneData( root.get() );
+
+    viewer.setUpViewOnSingleScreen(0);
+
+    return viewer.run();
+
+
+
+}
